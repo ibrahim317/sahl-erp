f6a4193de4d42196ac20f7db1468bc81f37b7ce9
ibrahim <zeroibrahim49@gmail.com>
2025-06-25 22:14:02 +0300

    removed Unit Access Request

--- sahl_erp/sahl_pms/doctype/unit_owner_access_permission/unit_owner_access_permission.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Softa Solutions and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Unit Owner Access Permission", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- sahl_erp/sahl_pms/doctype/unit_owner_access_permission/unit_owner_access_permission.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Softa Solutions and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class UnitOwnerAccessPermission(Document):
-	pass
