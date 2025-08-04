a31622380c48370037ed41d67ae86d10ba04fedc
ibrahim <zeroibrahim49@gmail.com>
2025-08-04 14:25:15 +0300

    feat: add package management and JavaScript bundling
    - Add package.json for JavaScript dependency management
    - Add sahl_erp.bundle.js for consolidated JavaScript functionality
    - Enable better asset management and optimization

---
+++ sahl_erp/public/js/sahl_erp.bundle.js
@@ -0,0 +1,13 @@
+// Patches (fixes to frappe, erpnext, etc.)
+import "./patches/sidebar.js";
+
+// Utils
+import "./utils/unit.js";
+import "./utils/events.js";
+
+// Desk Customizations (customizations to the desk UI)
+import "./desk_customizations/disable_listview_sidebar_by_defualt.js";
+import "./desk_customizations/toggle_search_bar_by_role.js";
+
+// Map Defaults
+import "./map_defaults.js";
